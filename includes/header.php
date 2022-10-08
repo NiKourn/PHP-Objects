50abf9cb479e81cd405a9d91558fb613a37eca6b
NiKourn <nikourn@gmail.com>
2022-10-08 15:59:46 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -6,37 +6,37 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 	<script src="assets/js/theme.js"></script>
 </head>
-<header>
-	<nav class="navbar navbar-expand-lg navbar-light bg-primary" style="background-color: #e3f2fd;">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="#">IT Conference</a>
-			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-				<div class="navbar-nav mr-auto">
-					<li class="nav-item"><a class="nav-link active" aria-current="page" href="homepage.php">Home</a></li>
-					<li class="nav-item"> <a class="nav-link" href="viewrecords.php">View Records</a></li>
-					<!--<li class="nav-item"> <a class="nav-link" href="admin-pane/main.php">Edit Site</a></li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Link</a>
-							<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-							  <li><a class="dropdown-item" href="#">Action</a></li>
-							  <li><a class="dropdown-item" href="#">Another action</a></li>
-							  <li><hr class="dropdown-divider"></li>
-							  <li><a class="dropdown-item" href="#">Something else here</a></li>
-							</ul>
-					  </li>-->
-				</div>
-				<div class="navbar-nav float-end">
-					<?php if(!isset($_SESSION['userid'])){  ?>
-						<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li><?php } else {?>
-					<li class="nav-item"><a class="nav-link" href="#"><span>Hello <?php echo $_SESSION['username']. ' - ' . $_SESSION['userid'];  ?></span></a></li>
-					<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a><?php } ?></li>
-				</div>
-			
-			</div>
-		</div>
-	</nav>
-</header>
+<!--<header>-->
+<!--	<nav class="navbar navbar-expand-lg navbar-light bg-primary" style="background-color: #e3f2fd;">-->
+<!--		<div class="container-fluid">-->
+<!--			<a class="navbar-brand" href="#">IT Conference</a>-->
+<!--			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--				<span class="navbar-toggler-icon"></span>-->
+<!--			</button>-->
+<!--			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
+<!--				<div class="navbar-nav mr-auto">-->
+<!--					<li class="nav-item"><a class="nav-link active" aria-current="page" href="homepage.php">Home</a></li>-->
+<!--					<li class="nav-item"> <a class="nav-link" href="viewrecords.php">View Records</a></li>-->
+<!--					<!--<li class="nav-item"> <a class="nav-link" href="admin-pane/main.php">Edit Site</a></li>-->
+<!--					<li class="nav-item dropdown">-->
+<!--						<a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">-->
+<!--							Link</a>-->
+<!--							<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">-->
+<!--							  <li><a class="dropdown-item" href="#">Action</a></li>-->
+<!--							  <li><a class="dropdown-item" href="#">Another action</a></li>-->
+<!--							  <li><hr class="dropdown-divider"></li>-->
+<!--							  <li><a class="dropdown-item" href="#">Something else here</a></li>-->
+<!--							</ul>-->
+<!--					  </li>-->-->
+<!--				</div>-->
+<!--				<div class="navbar-nav float-end">-->
+<!--					--><?php //if(!isset($_SESSION['userid'])){  ?>
+<!--						<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>--><?php //} else {?>
+<!--					<li class="nav-item"><a class="nav-link" href="#"><span>Hello --><?php //echo $_SESSION['username']. ' - ' . $_SESSION['userid'];  ?><!--</span></a></li>-->
+<!--					<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a>--><?php //} ?><!--</li>-->
+<!--				</div>-->
+<!--			-->
+<!--			</div>-->
+<!--		</div>-->
+<!--	</nav>-->
+<!--</header>-->
